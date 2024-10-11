f99f87f0cd65bedfb2f0fbca1c45fdf9f4c6a3cb
Biraj Sharma <bxs0556@mavs.uta.edu>
2024-10-11 11:37:53 -0500

    Final Solution

--- 11-strike_zone/solution.c
+++ 11-strike_zone/solution.c
@@ -3,24 +3,26 @@
 
 void print_bar(int size, float progress, int strike_zone) {
     int i, start, end;
-
+    
+    // Calculate the start and end of the strike zone
     start = round(size / 2.0 - strike_zone / 2.0);
     end = round(size / 2.0 + strike_zone / 2.0);
 
-    printf("start: %d, end: %d\n", start, end);
-
+    // Print the bar
     printf("|");
     for (i = 0; i < size; i++) {
         if (i == start) {
             printf("[");
         }
-        if (i == end) {
-            printf("]");
-        }
-        if (i < progress * size) {
+        if (i >= progress * size && i < end) {
+            printf(" ");
+        } else if (i < progress * size) {
             printf("=");
         } else {
             printf(" ");
+        }
+        if (i == end - 1) {
+            printf("]");
         }
     }
     printf("|\n");
@@ -29,6 +31,7 @@ void print_bar(int size, float progress, int strike_zone) {
 int main(int argc, char *argv[]) {
     print_bar(10, 0.5, 1);
     print_bar(10, 0.5, 2);
+    print_bar(10, 0.5, 4);
     print_bar(15, 0.3, 4);
     return 0;
 }
