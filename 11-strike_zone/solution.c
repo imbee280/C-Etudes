dc8c983f69908bb6e4d30cbeefd54a73052f41c2
Biraj Sharma <bxs0556@mavs.uta.edu>
2024-10-11 23:58:29 -0500

    Final

--- 11-strike_zone/solution.c
+++ 11-strike_zone/solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 void print_bar(int size, float progress, int strike_zone) {
     int i, start, end;
@@ -8,30 +9,40 @@ void print_bar(int size, float progress, int strike_zone) {
     start = round(size / 2.0 - strike_zone / 2.0);
     end = round(size / 2.0 + strike_zone / 2.0);
 
+    printf("start: %d, end: %d\n", start, end);
+
     // Print the bar
     printf("|");
     for (i = 0; i < size; i++) {
         if (i == start) {
             printf("[");
         }
-        if (i >= progress * size && i < end) {
-            printf(" ");
-        } else if (i < progress * size) {
+        if (i == end) {
+            printf("]");
+        } 
+        if (i < progress * size) {
             printf("=");
         } else {
             printf(" ");
-        }
-        if (i == end - 1) {
-            printf("]");
         }
     }
     printf("|\n");
 }
 
 int main(int argc, char *argv[]) {
-    print_bar(10, 0.5, 1);
-    print_bar(10, 0.5, 2);
-    print_bar(10, 0.5, 4);
-    print_bar(15, 0.3, 4);
+    
+    
+
+    int size = atoi(argv[1]);
+    float progress = atof(argv[2]);
+    int zone_width = atoi(argv[3]);
+
+    print_bar(size, progress, zone_width);
+
+    for (int i = 0; i < 100; i++) {
+        float progress = (float)i / 100;
+        print_bar(100, progress, 10);
+    }
+    
     return 0;
 }
