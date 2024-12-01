b80d4ddf413a88b88df76738f5313a93a2a0f13d
imbee280 <159008330+imbee280@users.noreply.github.com>
2024-12-01 15:23:35 -0600

    Update main.c

--- 13-strike_zone_2/main.c
+++ 13-strike_zone_2/main.c
@@ -1,16 +1,21 @@
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
-    // TODO: Verify command line arguments
+    // DONE: Verify command line arguments
+    if (argc != 5) {
+        printf("Usage: %s <limit> <fps> <bar_size> <strike_zone>\n", argv[0]);
+        return 1;
+    }
 
-    // TODO: Parse command line arguments
-    int limit = 0;
-    int fps = 0;
-    int bar_size = 0;
-    int strike_zone = 0;
+    // DONE: Parse command line arguments
+    int limit = atoi(argv[1]);
+    int fps = atoi(argv[2]);
+    int bar_size = atoi(argv[3]);
+    int strike_zone = atoi(argv[4]);
 
     clock_t start = clock();
     clock_t elapsed_time = clock() - start;
@@ -18,7 +23,12 @@ int main(int argc, char *argv[]) {
     while (elapsed_time < limit * CLOCKS_PER_SEC) {
         elapsed_time = clock() - start;
         if (elapsed_time % (CLOCKS_PER_SEC / fps) == 0) {
-            printf("%ld\n", elapsed_time * 1000 / CLOCKS_PER_SEC);
+            // Clear screen
+            printf("\033[2J\033[1;1H");
+            // system("clear"); // Slower than ANSI escape codes
+
+            // Print progress bar
+            print_bar(bar_size, (float) elapsed_time / (limit * CLOCKS_PER_SEC), strike_zone);
         }
     }
 }
